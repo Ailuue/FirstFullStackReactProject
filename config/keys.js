d0dbcb519545afa1429c863fa5a42498a9efb334
Alex Vervloet <alexvervloet@Alexs-MacBook-Pro.local>
2017-10-22 17:32:54 +0800

    finished auth flow

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+//credentials decision
+
+process.env.NODE_ENV === 'production' ? 
+module.exports = require('./prod') : 
+module.exports = require('./dev');
---
+++ config/prod.js
@@ -0,0 +1,9 @@
+//production keys
+module.exports = {
+  GoogleClientID: process.env.GOOGLE_CLIENT_ID,
+  GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+}
+
+mongodb://chen.chen.ling:candygo886@ds127065.mlab.com:27065/react-node-emails-prod
